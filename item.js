a8635badd2e02d4fc76fc1f4913a134b65220397
RaviVimala <saravanaravi95@gmail.com>
2025-10-05 19:09:39 +0530

    Add files via upload

--- item.js
+++ item.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const Router = express.Router();
-const Item = require('../Model/item');
+const mongoose = require("mongoose");
 
-
-//GET Method Running
-Router.get('/', async (req, res) => {
-    try {
-        const items = await Item.find();
-        res.json(items);
-    } catch (error) {
-        res.status(500).json({error: err.message});        
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
     }
 });
 
-//POST Method Running
-Router.post('/', async (req, res) => {
-    try {
-        const { name, quantity, price } = req.body;
-        const item = new Item ({ name, quantity, price });
-        await item.save();
-        res.status(201).json(item);
-    } catch (error) {
-        res.status(400).json({Error: err.message});
-    }
-});
-
-module.exports = Router;
+module.exports = mongoose.model('Item', itemSchema);
